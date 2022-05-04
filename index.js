5684ba3c4e4260894f4b615a026f93afa29dcb78
NahrulK <khayattullah73@gmail.com>
2022-05-04 11:41:24 +0800

    Connect Database

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://nahrul:nahrul123@getama-industry.phxdi.mongodb.net/getama?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5000, () => {
   console.log("Backend sedang berjalan!");
