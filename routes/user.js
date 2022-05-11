4f0993903b136328ad59a3976559abd2e02d5ea1
NahrulK <khayattullah73@gmail.com>
2022-05-11 14:50:03 +0800

    Login API

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,3 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("User Succesfull!");
-});
-
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  console.log(username);
-  res.send("Username anda adalah " + username);
-});
-
 module.exports = router;
